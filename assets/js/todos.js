9511593ff8e9b03faed1f9f9402fa477378a99f0
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-09-12 13:16:14 +0530

    Creating Todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 //Check off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation(); //to stop bubbling up the parent elements
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13)  { // 13 is charCode for "enter" key
+		//grabbing new todoText from input
+		var todoText = $(this).val();
+		$(this).val(""); //to clear the input
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
