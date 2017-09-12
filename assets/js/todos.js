3f51282c6aa19e7bcc4296ec52bf568e821bd6e2
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-09-12 12:08:51 +0530

    Deleting Todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click(function(){
 	$(this).toggleClass("completed");
 });
+
+//Click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation(); //to stop bubbling up the parent elements
+});
